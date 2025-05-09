a8ca47b4468c9cd533c7dc17b3c6101ce209e5be
nedelnik <p.nedelnik@email.cz>
2025-05-09 23:53:12 +0200

    WIP

--- src/ipython_loader.py
+++ src/ipython_loader.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pandas as pd
 
-from src.code_processing import parse_code_string  # Ensure parse_code_string is defined in utils.py or another module
+from src.code_processing import parse_code_string
 
 
 def load_items(ipython_path):
@@ -21,10 +21,52 @@ def load_items(ipython_path):
     items["solution"] = items["solution"].apply(lambda x: eval(x)[0][1]).apply(parse_code_string)
     items["democode"] = items["democode"].apply(lambda x: eval(x)[0][1]).apply(parse_code_string)
 
+    # order by difficulty
+    ps_order = [
+        "Proměnné a číselné výrazy",
+        "Cyklus for",
+        "Logické výrazy",
+        "Podmíněný příkaz (if): základy",
+        "Cyklus for s vnořenou podmínkou",
+        "Cyklus while",
+        "Podmíněný příkaz (if): těžší",
+        "Posloupnosti",
+        "Úpravy programů",
+        "Řízení výpočtu",
+        "Textové obrázky",
+        "Řetězce: základy",
+        "Seznamy",
+        "Řetězce: těžší",
+        "Seznamy a řetězce: vnořené",
+        "Slovníky",
+        "Záludné",
+    ]
+
+    # load problem sets
+    ps = pd.read_csv(ipython_path / "ps.csv", index_col=0, header=0, sep=";")
+    ps = ps[ps["url"].apply(lambda x: "interaktivni-python" in x)][["topic", "ordering"]]
+    ps.rename(columns={"ordering": "topic order"}, inplace=True)
+    ps["difficulty order"] = ps["topic"].apply(lambda x: ps_order.index(x))
+
+    # load mapping to items
+    item_to_ps = pd.read_csv(ipython_path / "ps_problem.csv", index_col=0, header=0, sep=";")
+    item_to_ps = item_to_ps[
+        item_to_ps["problem"].apply(lambda x: x in items.index) & item_to_ps["ps"].apply(lambda x: x in ps.index)
+    ]
+
+    # add to items
+    keep = items.columns.union(ps.columns).tolist() + ["id"]
+    items = (
+        items.reset_index(names="id")
+        .merge(item_to_ps, left_on="id", right_on="problem")
+        .merge(ps, left_on="ps", right_index=True)[keep]
+    )
+    items.set_index("id", inplace=True)
+
     return items
 
 
-def load_log(ipython_path, only_correct=True, only_final=True):
+def load_log(ipython_path):
     """Load and preprocess log data."""
     log = pd.read_csv(ipython_path / "log.csv", sep=";")
 
@@ -34,6 +76,9 @@ def load_log(ipython_path, only_correct=True, only_final=True):
     # Convert time column to datetime and correct data types
     log["time"] = pd.to_datetime(log["time"])
     log["correct"] = log["correct"].astype(bool)
+
+    # Sort by time
+    log.sort_values("time", inplace=True)
 
     # Drop problematic rows
     log.dropna(inplace=True)
@@ -45,13 +90,8 @@ def load_log(ipython_path, only_correct=True, only_final=True):
     # Decode submissions
     log["answer"] = log["answer"].apply(parse_code_string)
 
-    if only_correct:
-        # Keep only correct answers
-        log = log[log["correct"]]
-
-    if only_final:
-        # Keep only one answer per session
-        log = log.reset_index().groupby(["user", "item"], as_index=False).last().set_index("index")
+    # Keep only sessions lasting at least 1 minutes
+    log = log[log["responseTime"] >= 6e4]
 
     return log
 
@@ -100,6 +140,10 @@ def load_defects(data_path):
     # Create a dictionary mapping EduLint codes to defect indices
     code_to_defect_id = {val: idx for idx, val in defects["EduLint code"].explode().items()}
 
+    # Replace missing values with None
+    defects["code fix example"] = defects["code fix example"].fillna("")
+    defects["code example"] = defects["code example"].fillna("")
+
     return defects, code_to_defect_id
 
 
@@ -129,16 +173,13 @@ def vectorize_defects(messages, log):
     # Create a defect log matrix (binary presence of defects per log entry)
     defect_log = pd.crosstab(messages["log entry"], messages["defect"]).reindex(log.index, fill_value=0)
 
-    # Replace defect counts with presence (1 if present, 0 otherwise)
-    defect_log = (defect_log > 0).astype(int)
-
     return defect_log
 
 
-def load(ipython_path, data_path, only_correct=True, only_final=True):
+def load(ipython_path, data_path, only_correct=True, only_final=True, only_presence=True):
     """Load and preprocess data."""
     items = load_items(ipython_path)
-    log = load_log(ipython_path, only_correct=only_correct, only_final=True)
+    log = load_log(ipython_path)
     items, log = filter_items_and_log(items, log)
     defects, code_to_defect_id = load_defects(data_path)
     messages = load_messages(ipython_path, log, code_to_defect_id)
@@ -146,5 +187,22 @@ def load(ipython_path, data_path, only_correct=True, only_final=True):
 
     # Keep only detected defects
     defects = defects.loc[defects.index.isin(defect_log.columns)]
+
+    # optinal filtering is here at the end to ensure consistency in kept items and defects
+
+    # Keep only correct answers
+    if only_correct:
+        log = log[log["correct"]]
+
+    # Keep only one answer per session
+    if only_final:
+        log = log.reset_index().groupby(["user", "item"], as_index=False).last().set_index("index")
+
+    # Apply the filters to the defect log
+    defect_log = defect_log.loc[log.index]
+
+    # Replace defect counts with presence (1 if present, 0 otherwise)
+    if only_presence:
+        defect_log = (defect_log > 0).astype(int)
 
     return items, log, defects, defect_log, code_to_defect_id
